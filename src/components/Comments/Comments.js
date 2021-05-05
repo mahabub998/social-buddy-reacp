74790f89f95d74e29d36ee1908800ce626a0c9ea
mahabub alam <mahabu998@gmail.com>
2021-05-06 02:02:29 +0600

    last final

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Comments = (props) => {
-    const {id,name,email,body} = props.comment;
+    const {name,email} = props.comment;
     return (
         <div>
             <p>{name}</p>
